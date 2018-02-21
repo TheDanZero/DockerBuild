95822075f3ba05dd18bad3ae219f6f6ffdedfa2b
TheDanZero <36293127+TheDanZero@users.noreply.github.com>
2018-02-21 11:38:11 +0100

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -2,7 +2,7 @@
 
 cd DockerBuild
 
-docker build -t $DOCKER_REGISTRY_URL$JOB_NAME:$BUILD_ID .
+docker build -t $DOCKER_REGISTRY_URL$JOB_NAME:$BUILD_ID ./DockerBuild
 
 
 docker push $DOCKER_REGISTRY_URL$JOB_NAME:$BUILD_ID
