496b3865b5435de419a2ac0e72b184fae2344e38
TheDanZero <36293127+TheDanZero@users.noreply.github.com>
2018-02-21 11:17:32 +0100

    Update ShellScript.sh

--- DockerBuild/ShellScript.sh
+++ DockerBuild/ShellScript.sh
@@ -2,6 +2,6 @@
 
 
 docker build -t $JOB_NAME:$BUILD_ID .
-ALFRED="192.168.178.222:5000/alfred-registry/"
+STR="192.168.178.222:5000/alfred-registry/"
 docker tag $JOB_NAME:$BUILD_ID 192.168.178.222:5000/alfred-registry/$JOB_NAME:$BUILD_ID
-docker push $ALFRED/$JOB_NAME:$BUILD_ID
+docker push $STR/$JOB_NAME:$BUILD_ID
